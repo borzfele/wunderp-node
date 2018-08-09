bad310ab5c2ea018b0b34f701779ab50ee032c28
borzfele <borzfele@gmail.com>
2018-08-09 20:37:08 +0200

    hotfix

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Budgeteer', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/wunderp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
