0687ff8c32882e18119366e67b87b1c6799eba63
borzfele <borzfele@gmail.com>
2018-08-10 18:05:35 +0200

    authentication

--- wunderp.js
+++ wunderp.js
@@ -4,6 +4,10 @@ const {ObjectID} = require('mongodb');
 const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
+const {authenticate} = require('./middleware/authenticate');
+const {User} = require('./models/user');
+
+require('./config/config')
 
 const app = express();
 const port = process.env.PORT || 3000; 
@@ -12,6 +16,43 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Yo!');
+});
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+  
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+  
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+  
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+  
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, (err) => {
